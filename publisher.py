3f6d706a4dff26cccbad1f156c24c4d437568a13
alex <aleksandrshliakhovoi@gmail.com>
2025-02-26 22:00:58 +0200

    Add consumer

--- publisher.py
+++ publisher.py
@@ -1,6 +1,6 @@
 import time
-import logging
 from typing import TYPE_CHECKING
+import logging
 
 from config import(
     get_connection,
@@ -16,28 +16,23 @@ log = logging.getLogger(__name__)
 
 def produce_message(channel: "BlockingChannel") -> None:
     queue = channel.queue_declare(queue=MQ_ROUTING_KEY)
-    log.info("Declared queue message %s %s", MQ_ROUTING_KEY, queue)
-    message_body = f"Hello World! {time.time()}"
+    log.info("Declared queue message %r %s", MQ_ROUTING_KEY, queue)
+    message_body = f"Hello Worldf! {time.time()}"
     log.info("Publish message %s", message_body) # Publish messages
     channel.basic_publish(
-        exchange='',
-        routing_key=MQ_EXCHANGE,
-        body=MQ_ROUTING_KEY
+        exchange=MQ_EXCHANGE,
+        routing_key=MQ_ROUTING_KEY,
+        body=message_body
     )
     log.warning("Published message %s", message_body)
 
 def main():
-    configure_logging(level=logging.INFO)
+    configure_logging(level=logging.WARN)
     with get_connection() as connection:
         log.info("Created connection: %s", connection)
         with connection.channel() as channel:
             log.info("Created channel: %s", channel)
             produce_message(channel=channel)
-            produce_message(channel=channel)
-            produce_message(channel=channel)
-
-            while True:
-                pass
 
 if __name__ == "__main__":
     try:
